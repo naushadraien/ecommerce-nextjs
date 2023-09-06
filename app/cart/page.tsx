6b5b5a1ae16a214993350752c17c9fdff5280173
naushadraien <naushadraien5@gmail.com>
2023-09-06 13:35:48 +0545

    added cart section styling

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -24,28 +24,35 @@ const CartItem = () => {
     );
 
   return (
-    <div className="flex mt-20 overflow-hidden justify-center items-center">
-      <div className="container">
-        <h1 className="font-bold text-2xl flex justify-center items-center">
-          Cart Item
-        </h1>
+    <div className="flex mt-20 overflow-hidden lg:mx-10 flex-col">
+      <h1 className="font-bold text-2xl flex justify-center items-center">
+        Cart Item
+      </h1>
+      <div className="container grid grid-cols-2">
         {cart.cartItems.map((item, index) => (
-          <div key={index} className="my-10 flex justify-center items-center">
+          <div
+            key={index}
+            className="my-10 max-lg:mx-10 flex justify-center items-center max-lg:flex-col"
+          >
             <Image
               src={item.image}
               alt={item.title}
-              width={300}
-              height={300}
-              className="mb-4"
+              width={200}
+              height={200}
+              className="mb-4 lg:mx-16 hover:scale-110 duration-200"
             />
-            <div className="flex justify-center items-center flex-col w-36">
-              <p className="font-bold lg:text-lg text-md text-purple-700">
-                {item.title}
-              </p>
-              <p>{item.category}</p>
-              <p>$({item.price})</p>
+            <div className="flex flex-col w-40">
+              <div className="flex flex-col max-lg:justify-center max-lg:items-center">
+                <p className="font-bold lg:text-lg text-md text-purple-700 max-lg:ml-7">
+                  {item.title}
+                </p>
+                <p className="text-slate-500">{item.category}</p>
+                <p className="font-bold">RS. {item.price}</p>
+              </div>
+              {/* <div className=""> */}
+              {/* </div> */}
             </div>
-            <div className="flex flex-1 justify-center items-center flex-col">
+            <div className="flex flex-1 justify-center items-center flex-col ">
               <div className="flex justify-center items-center my-2">
                 <div className="cursor-pointer rounded-l bg-purple-700 text-white font-bold py-1 px-3.5 duration-100 hover:bg-purple-900 hover:text-blue-50">
                   +
@@ -60,7 +67,7 @@ const CartItem = () => {
               <div>
                 <button
                   onClick={() => handleRemove(item.id)}
-                  className="text-slate-700 px-2 rounded-md font-bold "
+                  className="text-red-500 hover:text-red-700 px-2 rounded-md font-bold"
                 >
                   Remove
                 </button>
@@ -69,31 +76,33 @@ const CartItem = () => {
           </div>
         ))}
       </div>
-      <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
-        <div className="mb-2 flex justify-between">
-          <p className="text-gray-700">Subtotal</p>
-          {/* <p className='text-gray-700'>Rs {cart.totalAmount.toFixed(2)}</p> */}
-        </div>
-        <div className="flex justify-between">
-          <p className="text-gray-700">Shipping</p>
-          <p className="text-gray-700">Rs 4.99</p>
-        </div>
-        <hr className="my-4" />
-        <div className="flex justify-between">
-          <p className="text-lg font-bold">Total</p>
-          <div className="">
-            <p className="mb-1 text-lg font-bold">
-              {/* Rs {(cart.totalAmount + 4.99).toFixed(2)} */}
-            </p>
-            <p className="text-sm text-gray-700">including VAT</p>
+      <div className="flex justify-center items-center">
+        <div className="h-full rounded-lg border bg-white p-6 shadow-md my-20 w-full md:w-[40%] lg:w-[30%]">
+          <div className="mb-2 flex justify-between">
+            <p className="text-gray-700">Subtotal</p>
+            {/* <p className='text-gray-700'>Rs {cart.totalAmount.toFixed(2)}</p> */}
           </div>
+          <div className="flex justify-between">
+            <p className="text-gray-700">Shipping</p>
+            <p className="text-gray-700">Rs 4.99</p>
+          </div>
+          <hr className="my-4" />
+          <div className="flex justify-between">
+            <p className="text-lg font-bold text-purple-700">Total</p>
+            <div className="">
+              <p className="mb-1 text-lg font-bold">
+                {/* Rs {(cart.totalAmount + 4.99).toFixed(2)} */}
+              </p>
+              <p className="text-sm text-gray-700">including VAT</p>
+            </div>
+          </div>
+          <button
+            className="mt-6 w-full bg-purple-700 py-1.5 font-medium text-blue-50 hover:bg-purple-900 duration-100 rounded-md"
+            onClick={() => alert("Checkout functionality not added!")}
+          >
+            Check out
+          </button>
         </div>
-        <button
-          className="mt-6 w-full bg-black py-1.5 font-medium text-blue-50 hover:bg-black/80"
-          onClick={() => alert("Checkout functionality not added!")}
-        >
-          Check out
-        </button>
       </div>
     </div>
   );
