df9e72638935c9ace01e71a88d3012e8bbf829ac
naushadraien <naushadraien5@gmail.com>
2023-09-06 23:11:04 +0545

    search results styling added

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,17 +1,21 @@
 "use client";
-import { removeAProductFromCart } from "@/redux/slice/cartSlice";
+import {
+  CartItems,
+  addToCart,
+  deleteProductsFromCart,
+  removeAProductFromCart,
+} from "@/redux/slice/cartSlice";
 import { RootState } from "@/redux/store/store";
 import Image from "next/image";
 import Link from "next/link";
-import { useDispatch, useSelector } from "react-redux";
 import toast from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
 const CartItem = () => {
   const dispatch = useDispatch();
   const { cart } = useSelector((state: RootState) => state);
 
-  const handleRemove = (id: number) => {
-    dispatch(removeAProductFromCart(id));
-    toast.error("Product removed from cart");
+  const handleAdd = (item: CartItems) => {
+    dispatch(addToCart({ ...item, quantity: 1 }));
   };
 
   if (!cart.cartItems.length)
@@ -54,19 +58,28 @@ const CartItem = () => {
             </div>
             <div className="flex flex-1 justify-center items-center flex-col ">
               <div className="flex justify-center items-center my-2">
-                <div className="cursor-pointer rounded-l bg-purple-700 text-white font-bold py-1 px-3.5 duration-100 hover:bg-purple-900 hover:text-blue-50">
+                <div
+                  className="cursor-pointer rounded-l bg-purple-700 text-white font-bold py-1 px-3.5 duration-100 hover:bg-purple-900 hover:text-blue-50"
+                  onClick={() => handleAdd(item)}
+                >
                   +
                 </div>
                 <p className="h-8 w-8 border bg-purple-200 text-center font-bold text-sm flex justify-center items-center">
-                  4
+                  {item.quantity}
                 </p>
-                <div className="cursor-pointer rounded-r bg-purple-700 text-white font-bold py-1 px-3 duration-100 hover:bg-purple-900 hover:text-blue-50">
+                <div
+                  className="cursor-pointer rounded-r bg-purple-700 text-white font-bold py-1 px-3 duration-100 hover:bg-purple-900 hover:text-blue-50"
+                  onClick={() => dispatch(removeAProductFromCart(item.id))}
+                >
                   -
                 </div>
               </div>
               <div>
                 <button
-                  onClick={() => handleRemove(item.id)}
+                  onClick={() =>
+                    dispatch(deleteProductsFromCart(item.id)) &&
+                    toast.error("Item removed from cart")
+                  }
                   className="text-red-500 hover:text-red-700 px-2 rounded-md font-bold"
                 >
                   Remove
@@ -80,25 +93,29 @@ const CartItem = () => {
         <div className="h-full rounded-lg border bg-white p-6 shadow-md my-20 w-full md:w-[40%] lg:w-[30%]">
           <div className="mb-2 flex justify-between">
             <p className="text-gray-700">Subtotal</p>
-            {/* <p className='text-gray-700'>Rs {cart.totalAmount.toFixed(2)}</p> */}
+            <p className="text-gray-700">
+              Rs {cart.totalPriceofProduct.toFixed(2)}
+            </p>
           </div>
           <div className="flex justify-between">
             <p className="text-gray-700">Shipping</p>
-            <p className="text-gray-700">Rs 4.99</p>
+            <p className="text-gray-700">Rs 10.99</p>
           </div>
           <hr className="my-4" />
           <div className="flex justify-between">
             <p className="text-lg font-bold text-purple-700">Total</p>
             <div className="">
               <p className="mb-1 text-lg font-bold">
-                {/* Rs {(cart.totalAmount + 4.99).toFixed(2)} */}
+                Rs {(cart.totalPriceofProduct + 10.99).toFixed(2)}
               </p>
               <p className="text-sm text-gray-700">including VAT</p>
             </div>
           </div>
           <button
             className="mt-6 w-full bg-purple-700 py-1.5 font-medium text-blue-50 hover:bg-purple-900 duration-100 rounded-md"
-            onClick={() => alert("Checkout functionality not added!")}
+            onClick={() =>
+              toast.error("Checkout functionality will be added soon!")
+            }
           >
             Check out
           </button>
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -14,25 +14,35 @@ const Search = () => {
 
   return (
     <>
-      {startupPokemon &&
-        startupPokemon
-          .filter((product) => {
-            if (search.length === 0) {
-              return product; //If searchData is an empty string, it returns all the productss of the users array.
-            } else {
-              return product.title.toLowerCase().includes(search.toLowerCase()); //If searchData has a value, it returns only those productss whose name property includes the search string. The comparison is case-insensitive because both the name and searchData strings are converted to lowercase using the toLowerCase() method before the comparison.
-            }
-          })
-          .map((product) => {
-            return (
-              <div
-                key={product.id}
-                className="flex justify-center mt-20 -mb-10"
-              >
-                <Product {...product} />
-              </div>
-            );
-          })}
+      {" "}
+      <div className="flex justify-center items-center mt-10 font-bold text-xl">
+        <p>
+          Search Result for: <span className="text-purple-700"> {search}</span>
+        </p>
+      </div>
+      <div className="flex justify-center items-center max-lg:flex-col flex-wrap">
+        {startupPokemon &&
+          startupPokemon
+            .filter((product) => {
+              if (search.length === 0) {
+                return product; //If searchData is an empty string, it returns all the productss of the users array.
+              } else {
+                return product.title
+                  .toLowerCase()
+                  .includes(search.toLowerCase()); //If searchData has a value, it returns only those productss whose name property includes the search string. The comparison is case-insensitive because both the name and searchData strings are converted to lowercase using the toLowerCase() method before the comparison.
+              }
+            })
+            .map((product) => {
+              return (
+                <div
+                  key={product.id}
+                  className="flex justify-between mx-5 my-8"
+                >
+                  <Product {...product} />
+                </div>
+              );
+            })}
+      </div>
     </>
   );
 };
