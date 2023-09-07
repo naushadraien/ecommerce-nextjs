523cdf6f57bbe115529e7202bc3e085cb0c0c733
naushadraien <naushadraien5@gmail.com>
2023-09-07 13:17:08 +0545

    added no search result if product is not found

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -10,38 +10,30 @@ const Search = () => {
     (state: RootState) => state.search
   );
 
-  if (!search) return router.push("/"); //if there is no search, it will redirect to the home page
+  if (!search) return router.push("/"); // If there is no search, it will redirect to the home page
+
+  // Filter the products based on the search criteria
+  const filteredProducts = startupPokemon.filter((product) => {
+    return product.title.toLowerCase().includes(search.toLowerCase());
+  });
 
   return (
     <>
-      {" "}
       <div className="flex justify-center items-center mt-10 font-bold text-xl">
         <p>
           Search Result for: <span className="text-purple-700"> {search}</span>
         </p>
       </div>
       <div className="flex justify-center items-center max-lg:flex-col flex-wrap">
-        {startupPokemon &&
-          startupPokemon
-            .filter((product) => {
-              if (search.length === 0) {
-                return product; //If searchData is an empty string, it returns all the productss of the users array.
-              } else {
-                return product.title
-                  .toLowerCase()
-                  .includes(search.toLowerCase()); //If searchData has a value, it returns only those productss whose name property includes the search string. The comparison is case-insensitive because both the name and searchData strings are converted to lowercase using the toLowerCase() method before the comparison.
-              }
-            })
-            .map((product) => {
-              return (
-                <div
-                  key={product.id}
-                  className="flex justify-between mx-5 my-8"
-                >
-                  <Product {...product} />
-                </div>
-              );
-            })}
+        {filteredProducts.length === 0 ? (
+          <p className="my-20">No results found</p>
+        ) : (
+          filteredProducts.map((product) => (
+            <div key={product.id} className="flex justify-between mx-5 my-8">
+              <Product {...product} />
+            </div>
+          ))
+        )}
       </div>
     </>
   );
